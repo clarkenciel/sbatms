8ff8810fe7e2ebed5e6c83039366b0ce54da7e73
clarkenciel <danny@clarkenciel.com>
2015-12-02 09:27:10 -0800

    removing freq info

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,12 +1,16 @@
 #include "../lib/pulse.h"
+
 // pitch
 int n_freqs = 6;
 //float freqs[] = { 955, 1911, 1432, 1276, 1073, 758 };
 float freqs[] = { 0.1, 0.2, 0.3, 0.4 };
+double next_freq;
 int pitch_idx = 0;
 
 struct SquarePulse sp;
 struct SquarePulse sp2;
+
+double now;
 
 void 
 setup()
@@ -18,25 +22,16 @@ setup()
 void 
 loop()
 {
-  double now = micros();
-  if (now >= sp.attack_time) {
-    playSquarePulse(&sp);
-  }
-  if (now >= sp.end_time) {
-    muteSquarePulse(&sp);
-    sp.freq = freqs[(pitch_idx = (pitch_idx + 1) % n_freqs)];
-    rescheduleSquarePulse(&sp, 0.25);
-  }
+  now = micros();
+  playSquarePulse(&sp);
+  if (now >= next_freq) 
+  sp.freq = freqs[(pitch_idx = (pitch_idx + 1) % n_freqs)];
+  rescheduleSquarePulse(&sp, 0.25);
 
   // playing with a "second voice"
-  // if (now >= sp2.attack_time) {
-  //   playSquarePulse(&sp2);
-  // }
+  // playSquarePulse(&sp2);
 
-  // if (now >= sp2.end_time) {
-  //   muteSquarePulse(&sp2);
-  //   sp2.freq = freqs[(pitch_idx = (pitch_idx + 2) % n_freqs)];
-  //   rescheduleSquarePulse(&sp2, 0.33);
-  // }
+  // sp2.freq = freqs[(pitch_idx = (pitch_idx + 2) % n_freqs)];
+  // rescheduleSquarePulse(&sp2, 0.33);
 }
 
